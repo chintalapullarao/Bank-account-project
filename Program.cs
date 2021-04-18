7417d78109fd50a1b3b517986e625066af984695
chintala <82748943+chintalapullarao@users.noreply.github.com>
2021-04-18 20:47:08 +0530

    Add files via upload
     Object Oriented programming organizes code by creating types in the form of classes. These classes contain the code that represents a specific entity. The BankAccount class represents a bank account. The code implements specific operations through methods and properties. In this tutorial, the bank account supports this behavior:
    
    It has a 10-digit number that uniquely identifies the bank account.
    It has a string that stores the name or names of the owners.
    The balance can be retrieved.
    It accepts deposits.
    It accepts withdrawals.
    The initial balance must be positive.
    Withdrawals cannot result in a negative balance.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bankproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Bankaccount("dev", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "mobile recharge");
+            account.MakeWithdrawal(100, DateTime.Now, "groceries ");
+            account.MakeWithdrawal(150, DateTime.Now, " recharger");
+            account.MakeWithdrawal(40, DateTime.Now, "mobile recharge");
+            account.MakeDeposit(100, DateTime.Now, "GST");
+         
+            Console.WriteLine(account.GetAccountHistory());
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
